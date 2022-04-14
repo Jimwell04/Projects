45d3d23be5faa10a54e6cbb07083de86c877d66f
Jimwell040 <jimwellibay040@gmail.com>
2022-04-14 12:35:46 +0800

    Add new project in Code programming using p5.js

---
+++ Web Projects/Code programming using p5.js/Object Communication/bubble.js
@@ -0,0 +1,113 @@
+class bubble {
+  
+  constructor(x = random(width), y = height, rds = 50) {
+    
+    
+    this.x = x
+    this.y = y
+    this.w = rds;
+    this.h = rds;
+    this.rds = rds;
+    
+    this.r = 255;
+    this.g = 255;
+    this.b = 255;
+    this.a = 255;
+    
+    this.sw = 1;
+    
+  }
+  
+  show() {
+    
+    noFill();
+    stroke(this.r, this.g, this.b, this.a);
+    strokeWeight(this.sw);
+    ellipse(this.x, this.y, this.w, this.h);
+    
+  }
+  
+  animate(speed = 2) {
+    
+    this.y = this.y - speed;
+    this.x = this.x - random(-5,5);
+    
+    if (this.x < 0) {
+      
+    this.x = this.x * -5;
+      
+    } else if (this.x > width) {
+      
+      this.x = this.x - 10;
+      
+    }
+    
+    if (this.y < 0) {
+      
+      this.y = height;
+      
+    }
+    
+  }
+  
+  
+  color(r,g,b,a = 255) {
+    
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.a = a;
+    
+  }
+  
+  strokeWeight(sw) {
+    
+    this.sw = sw
+    
+  }
+  
+  position(x,y) {
+    
+    this.x = x;
+    this.y = y;
+    
+  }
+  
+  size(w,h) {
+    
+    this.w = w;
+    this.h = h;
+    
+  }
+  
+  click() {
+    
+    let distance = dist(mouseX, mouseY, this.x, this.y);
+    
+    if (distance < this.rds ) {
+      
+      
+      return true;
+    } else {
+      
+      return false;
+    }
+    
+  }
+  
+  intersects(object) {
+    
+    let distance = dist(this.x, this.y, object.x, object.y);
+    
+    if (distance < this.rds + object.rds) {
+      
+      return true;
+    } else {
+      
+      return false;
+    }
+    
+  }
+  
+}
+
---
+++ Web Projects/Code programming using p5.js/Object Communication/sketch.js
@@ -0,0 +1,80 @@
+let bubbles = [];
+
+function setup() {
+  
+  createCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed() {
+  
+  let click;
+  let index;
+  
+    for (let bubble of bubbles) {
+      
+      if (bubble.click()) {
+        
+        click = true;
+        index = bubbles.indexOf(bubble);
+        
+      } else {
+        
+        click = false;
+        
+      }
+      
+    }
+    
+    if (click) {
+      
+      bubbles.splice(index,1);
+      
+    } else {
+      
+      let rds = random(50, 100);
+      //               min max
+      let b = new bubble(mouseX, mouseY, rds);
+      
+      bubbles.push(b);
+      
+      print(bubbles.length);
+      
+    }
+    
+}
+
+function draw() {
+  
+  background(0, 155, 255);
+  
+  for (let bubble of bubbles) {
+    
+    let intersects;
+    
+    bubble.show();
+    bubble.animate(  1  );
+    //             speed
+    
+    for (let other of bubbles) {
+      
+      if (bubble !== other && bubble.intersects(other)) {
+        
+        intersects = true;
+        
+      } 
+      
+    }
+    
+    if (intersects) {
+    
+      bubble.color(255,0,0);
+    
+    } else {
+      
+      bubble.color(255,255,255);
+      
+    }
+    
+  }
+  
+}
